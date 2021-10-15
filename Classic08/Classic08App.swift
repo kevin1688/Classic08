48c3d309c4b9ffa5830891e10149cf49475ad991
kb18k <jackexi@gmail.com>
2021-10-15 19:55:33 +0800

    Initial Commit

---
+++ Classic08/Classic08App.swift
@@ -0,0 +1,17 @@
+//
+//  Classic08App.swift
+//  Classic08
+//
+//  Created by kai wen chen on 2021/10/15.
+//
+
+import SwiftUI
+
+@main
+struct Classic08App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
